0b6ef03d91194097e76b19198079fea233a85629
TigranKh21 <tigran.khachaturyants@gmail.com>
2023-11-21 00:51:26 +0200

    change language feature added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { langs } from './js/lang';
+
 // --------------------Modal--------------------
 
 const refs = {
@@ -7,21 +9,53 @@ const refs = {
   loginEmailEl: document.querySelector('.js-loginEmail'),
   loginPassword: document.querySelector('.js-loginPassword'),
   loginFormEl: document.querySelector('.js-modalForm'),
-  testMeBtn: document.querySelector('.js-testMeBtn'),
+  langBtn: document.querySelector('[data-modal-lang]'),
+  changeLangEl: document.querySelector('.js-langBtn'),
 };
+
+const allLangs = ['en', 'ua'];
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.loginFormEl.addEventListener('submit', onLogin);
+//refs.langBtn.addEventListener('click', onLangModal);
+refs.changeLangEl.addEventListener('change', onChangeURLLanguage);
 
 function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
 }
-
-refs.loginFormEl.addEventListener('submit', onLogin);
 
 function onLogin(e) {
   e.preventDefault();
   const email = e.currentTarget.email.value;
   const password = e.currentTarget.password.value;
   console.log(email, password);
+}
+
+/* function onLangModal() {
+  pass;
+}
+ */
+function onChangeURLLanguage() {
+  let lang = refs.changeLangEl.value;
+  location.href = window.location.pathname + '#' + lang;
+  onChangeLanguage();
+  //location.reload();
+}
+
+function onChangeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  if (!allLangs.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    //location.reload();
+  }
+  refs.changeLangEl.value = hash;
+  document.querySelector('title').innerHTML = langs['pageName'][hash];
+  for (let key in langs) {
+    const elem = document.querySelector('.lang-' + key);
+    if (elem) {
+      elem.innerHTML = langs[key][hash];
+    }
+  }
 }
---
+++ src/js/lang.js
@@ -0,0 +1,134 @@
+export const langs = {
+  pageName: {
+    en: 'Roca CRM',
+    ua: 'Roca: Система обліку роботи Представництва',
+  },
+  headerTitle: {
+    en: 'Representative Office in Ukraine',
+    ua: 'Представництво в Україні',
+  },
+  loginBtnText: {
+    en: 'Login',
+    ua: 'Логін',
+  },
+  posTitle: {
+    en: 'POS',
+    ua: 'Точки продажу',
+  },
+  distributors: {
+    en: 'Distributors',
+    ua: "Дістриб'ютори",
+  },
+  retail: {
+    en: 'Retail',
+    ua: 'Роздріб',
+  },
+  eComm: {
+    en: 'E-commerce',
+    ua: 'Інтернет-магазини',
+  },
+  diy: {
+    en: 'DIY',
+    ua: 'Маркети',
+  },
+  prices: {
+    en: 'Prices',
+    ua: 'Прайси',
+  },
+  mrktng: {
+    en: 'Marketing',
+    ua: 'Маркетинг',
+  },
+  adv: {
+    en: 'Advertisement',
+    ua: 'Реклама',
+  },
+  equip: {
+    en: 'POS equipment',
+    ua: 'Експозітори',
+  },
+  fairs: {
+    en: 'Fairs',
+    ua: 'Виставки',
+  },
+  gifts: {
+    en: 'Presents',
+    ua: 'Подарунки',
+  },
+  service: {
+    en: 'Service',
+    ua: 'Сервіс',
+  },
+  tManuals: {
+    en: 'Technical manuals',
+    ua: 'Технічні інструкції',
+  },
+  parts: {
+    en: 'Spare parts',
+    ua: 'Запчастини',
+  },
+  claims: {
+    en: 'Claims',
+    ua: 'Рекламації',
+  },
+  plumbers: {
+    en: 'Service companies and plumbers',
+    ua: 'Сервісні компанії та інженери',
+  },
+  projects: {
+    en: 'Projects',
+    ua: 'Проєкти',
+  },
+  office: {
+    en: 'Office',
+    ua: 'Офіс',
+  },
+  employees: {
+    en: 'Employees',
+    ua: 'Співробітники',
+  },
+  cars: {
+    en: 'Cars',
+    ua: 'Автівки',
+  },
+  warehouse: {
+    en: 'Warehouse',
+    ua: 'Склад',
+  },
+  loginModalTitle: {
+    en: 'Loign to your account',
+    ua: 'Увійдіть в свій акаунт',
+  },
+  email: {
+    en: 'Email',
+    ua: 'Електронна пошта',
+  },
+  pass: {
+    en: 'password',
+    ua: 'пароль',
+  },
+  antey: {
+    en: 'Antey',
+    ua: 'Антей',
+  },
+  ns: {
+    en: 'Novyi Svit',
+    ua: 'Новий Світ',
+  },
+  ubm: {
+    en: 'Ukrbudmarket',
+    ua: 'Укрбудмркет',
+  },
+  laterem: {
+    en: 'Laterem',
+    ua: 'Латерем',
+  },
+  agromat: {
+    en: 'Agromat',
+    ua: 'Агромат',
+  },
+  akvademi: {
+    en: 'Akvademi',
+    ua: 'Аквадемі',
+  },
+};
